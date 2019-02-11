0b98f5dc345e3d4eea1fc60dd3fb9b68a1a5d47e
Natalia Stanko <anithaly@gmail.com>
2019-02-11 13:50:12 +0000

    add while loop with condition example

--- adding_numbers_from_user_input.rb
+++ adding_numbers_from_user_input.rb
@@ -6,6 +6,7 @@ sum = 0
 # get numbers from user input/stdin
 # chop the last char (a line break \n)
 until (char = gets.chomp) == 'q'
+# or while (char = gets.chomp) != 'q'
   sum += char.to_i # make sure it is integer
 end
 
