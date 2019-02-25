c9ef1aa192a103dfe8d1d9d7ba173b5d88e95c3c
Natalia Stanko <anithaly@gmail.com>
2019-02-25 02:37:35 +0000

    add example of GET request query format

--- hash_extraction.rb
+++ hash_extraction.rb
@@ -1,17 +1,17 @@
 require 'HTTParty'
 require 'json'
 
-filename = 'sciencemuseum_pretty.json'
+filename = 'stethoscope_london_sciencemuseum_pretty.json'
 
 unless File.file? filename 
   # Make a HTTP request to UK science museum collections
-  response = HTTParty.get('https://collection.sciencemuseum.org.uk/search/objects',
-    query: {
-      categories: 'clinical-diagnosis',
-      object_type: 'stethoscope',
-      'date[to]': 1900,
-      places: 'New York'
-    },
+  response = HTTParty.get('https://collection.sciencemuseum.org.uk/search/objects?categories=clinical-diagnosis&places=london&date[to]=1900&object_type=stethoscope',
+    # query: {
+    #   categories: 'clinical-diagnosis',
+    #   object_type: 'stethoscope',
+    #   'date[to]': 1900,
+    #   places: 'New York'
+    # },
     headers: {Accept: 'application/json'})
 
   # save reasult to json file with pretty format
@@ -22,5 +22,5 @@ end
 
 # read json file
 data_hash = JSON.parse(File.read filename)
-# extract link to the stethoscope
-puts data_hash.dig('data').first.dig('links', 'self')
+# extract link to each stethoscope
+data_hash.dig('data').each{ |stethoscope| puts stethoscope.dig('links', 'self') }
